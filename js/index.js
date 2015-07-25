f396b91aa51cfffb01c26bd8d60fbc5db862211a
Evan Xd <itoyxd@gmail.com>
2015-07-25 23:56:01 +0800

    Remove useless comment

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@
   var options = { baudRate: 57600 };
   var port = new ChromeUsbSerialport(path, options, true);
   var board = new five.Board({ port: port, repl: false });
-  // FIXME: Cannot connect the board.
   board.on('ready', function() {
     var led = new five.Led(7);
     led.blink(300);
