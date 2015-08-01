8d36d898bedbb7ef7a630e5835a25df3d8afe6fc
Evan Tseng <evan@tseng.io>
2015-08-01 18:03:37 +0800

    Remove the specific path of arduino device

--- js/board.js
+++ js/board.js
@@ -1,13 +1,12 @@
 /* global five, ChromeUsbSerialport */
 'use strict';
 
-(function(exports) {
-  var path = '/dev/cu.usbmodem1411';
+(function() {
   var options = {
   	baudRate: 57600,
   	extensionId: 'neponigbmdndahdonomcdimneongpnim'
   };
-  var port = new ChromeUsbSerialport(path, options, true);
+  var port = new ChromeUsbSerialport(null, options, true);
   var board = new five.Board({ port: port, repl: false });
 
   board.on('ready', function() {
@@ -15,6 +14,4 @@
   });
 
   window.dispatchEvent(new CustomEvent('boardconnecting'));
-
-  exports.arduino = board;
-}(window));
+}());
